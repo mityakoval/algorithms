4f0dac666716d0f7585dbdfc6a06383760b09789
mityakoval <dima.alex.koval@gmail.com>
2017-03-17 09:56:15 +0100

    add reading parameters from command line

--- union-find/script.rb
+++ union-find/script.rb
@@ -1,6 +1,9 @@
 require_relative 'percolation/percolation_stats.rb'
 
-mc = PercolationStats.new(200, 100)
+n = ARGV[0].to_i
+trials = ARGV[1].to_i
+
+mc = PercolationStats.new(n, trials)
 
 mc.print_results
 
